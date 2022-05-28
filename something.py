a61a6e07e1524caf1d1914fc35916829f49d776e
George <george@dataprax.is>
2022-05-28 22:54:00 +0100

    some commit 2

--- something.py
+++ something.py
@@ -9,6 +9,7 @@ def main() -> int:
     """Main function for pylint."""
     # Comment for the sake of it.
     # Another one.
+    # Another one...
     print("this is something")
     print("fail black")
     x_for_pylint = 12
