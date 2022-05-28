957cb305d90659923c11d9636c44e38906ceabc2
George <george@dataprax.is>
2022-05-28 22:58:39 +0100

    some commit 4

--- something.py
+++ something.py
@@ -7,9 +7,6 @@ This is a docstring which does nothing other than please a linter.
 
 def main() -> int:
     """Main function for pylint."""
-    # Comment for the sake of it.
-    # Another one.
-    # Another one...
     print("this is something")
     print("fail black")
     x_for_pylint = 12
