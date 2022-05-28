6851506c775f75fe061e804e83fe69d10550a0bd
George <george@dataprax.is>
2022-05-28 22:41:49 +0100

    something

--- something.py
+++ something.py
@@ -1,6 +1,13 @@
+"""
+Adding module docstring to shut pylint up.
+
+This is a docstring which does nothing other than please a linter.
+"""
+
+
 def main() -> int:
     print("this is something")
-    print('fail black')
+    print("fail black")
     x = 12
     return 0
 
