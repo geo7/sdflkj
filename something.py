d7b449457dd8485fd47ece1d9681e6fb3aec5151
George <george@dataprax.is>
2022-05-28 22:43:21 +0100

    sdf

--- something.py
+++ something.py
@@ -6,9 +6,11 @@ This is a docstring which does nothing other than please a linter.
 
 
 def main() -> int:
+    """Main function for pylint."""
     print("this is something")
     print("fail black")
-    x = 12
+    x_for_pylint = 12
+    del x_for_pylint
     return 0
 
 
