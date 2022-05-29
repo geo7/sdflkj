ddcbae76c2c90f98432420e64bb1dcb931bef049
geo7 <georgelenton@gmail.com>
2022-05-29 12:55:44 +0100

    update 9

--- tests/test_util.py
+++ tests/test_util.py
@@ -1,11 +1,11 @@
-'''
+"""
 Example tests for this package.
 
 These are some example tests.
-'''
+"""
 from sdflkj import util
 
 
 def test_add():
-    '''Just testing this method.'''
+    """Just testing this method."""
     assert util.add(1, 2) == 3
